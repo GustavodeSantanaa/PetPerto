e06dae59f1dab8d35f32cb35f0d344f7291f7e8e
gustavodesantana <santana.dev@hotmail.com>
2024-07-10 22:42:46 -0300

    Primeiro commit

---
+++ src/main/java/gustavo/santana/PetPerto/PetPertoApplication.java
@@ -0,0 +1,13 @@
+package gustavo.santana.PetPerto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetPertoApplication.class, args);
+	}
+
+}
